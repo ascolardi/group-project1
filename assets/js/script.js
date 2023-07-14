1752a79042439fe0f20f8ea69eca196e6c90b4e1
Aidan-Windebank <ajwindybanks@gmail.com>
2023-07-14 14:29:47 -0500

    added some stuff with local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,30 +3,30 @@ var fetchButton = document.getElementById('button');
 
 function getApi() {
   // fetch request gets a list of all the repos for the node.js organization
-  var requestUrl = 'https://www.themealdb.com/api/json/v1/1/filter.php?i=beef';
+  var requestUrl = 'https://www.themealdb.com/api/json/v1/1/filter.php?i=lamb';
 
   fetch(requestUrl)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      console.log(data.meals)
+      // console.log(data.meals)
       for (var i = 0; i < data.meals.length; i++) {
         var mealId = data.meals[i].idMeal
         var meals = data.meals[i].strMeal
         var mealImage = data.meals[i].strMealThumb
         
       
-        console.log(meals)
-        console.log(mealImage)
-        console.log(mealId)
+        // console.log(meals)
+        // console.log(mealImage)
+        // console.log(mealId)
       
         function showStored() {
             
             var newRow = `<tr>
                             <td> ${meals}
                             <td> <img src = "${mealImage}" id= "meal-image">
-                            <td> <button type="button" class="btn btn-primary">See Recipe</button>
+                            <td> <button id="recipe-button" type="button" class="btn btn-primary">See Recipe</button>
                           </tr>`
             $('#infoTable tbody').append(newRow);
            
@@ -36,9 +36,18 @@ function getApi() {
 
       }
     
+      $(data).each(function(index, meal){
+        console.log(meal);
+        var buttonClick = $('#recipe-button');
+        console.log(buttonClick);
+        $(buttonClick).on("click", function(){
+          // var mealLookUp = $(data.meals[i]);
+          console.log(mealId);
+          localStorage.setItem(index, mealId);
+        })
+      })
     })
-}
-    
+  }
 
         
 
