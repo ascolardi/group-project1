3fe4fec7a62022f6f1aefa35546bcba4165eaaa0
Aidan-Windebank <ajwindybanks@gmail.com>
2023-07-17 10:07:47 -0500

    added stuff from group members

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ var fetchButton = document.getElementById('button');
 
 function getApi() {
   // fetch request gets a list of all the repos for the node.js organization
-  var requestUrl = 'https://www.themealdb.com/api/json/v1/1/filter.php?i=lamb';
+  var requestUrl = 'https://www.themealdb.com/api/json/v1/1/filter.php?i=beef';
 
   fetch(requestUrl)
     .then(function (response) {
